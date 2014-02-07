a9dc142f7c0f4cc9545185cca098e9e6c7ea8ebc
marushkevych <marushkevych@gmail.com>
2014-02-06 21:03:45 -0500

    deleteing unused mock

--- spec/cmisSessionMock.js
+++
@@ -1,7 +0,0 @@
-var CmisRequestMock = require('./CmisRequestMock');
-
-exports.setCredentials = function(username, password){};
-
-exports.loadRepositories = function(){ return new CmisRequestMock(true)};
-
-exports.getObjectByPath = function(){};
