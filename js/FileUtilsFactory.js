30ef4681f45f0f09f9b20992d1fa5a5aa6ca30d9
marushkevych <marushkevych@gmail.com>
2014-03-03 13:19:18 -0500

    download: streaming file only once

--- js/FileUtilsFactory.js
+++ js/FileUtilsFactory.js
@@ -10,6 +10,8 @@ var url = require('url');
 var fs = require('fs');
 var grunt = require('grunt');
 var crypto = require('crypto');
+var Writable = require('stream').Writable;
+var util = require('util');
 /**
  * Factory method creates FileUtils object.
  * 
@@ -38,71 +40,69 @@ var crypto = require('crypto');
  * 
  */
 module.exports = function(cmisSession, options) {
-    
-    function getRemoteData(objectId, callback){
+
+    function getRemoteData(objectId, callback) {
         var URL = cmisSession.getContentStreamURL(objectId);
         var requestOptions = url.parse(URL);
         requestOptions.auth = options.username + ':' + options.password;
-        http.get(requestOptions, function(response){
+        http.get(requestOptions, function(response) {
             callback(null, response);
         }).on('error', function(e) {
             callback(e.message);
         });
     }
-    
-    function compare(filePath, objectId, callback){
-        getCheckSum(fs.createReadStream(filePath), function(err1, localCheckSum){
-            if(err1) {
+
+    function compare(data, filePath, callback) {
+        getCheckSum(data, function(err2, remoteCheckSum) {
+            if (err2) {
                 callback(null, false);
                 return;
             }
+            //console.log('got remote checksum', remoteCheckSum);
             
-            //console.log('got local checksum', localCheckSum);
             
-            getRemoteData(objectId, function(err, data){
-                
-                getCheckSum(data, function(err2, remoteCheckSum){
-                    if(err2) {
-                        callback(null, false);
-                        return;
-                    }
-                    //console.log('got remote checksum', remoteCheckSum);
-                    callback(null, localCheckSum === remoteCheckSum);
-                });
-            })
-            
+            getCheckSum(fs.createReadStream(filePath), function(err1, localCheckSum) {
+                if (err1) {
+                    callback(null, false);
+                    return;
+                }
+
+                //console.log('got local checksum', localCheckSum);
+                callback(null, localCheckSum === remoteCheckSum);
+            });
             
         });
     }
-    
+
     function getCheckSum(stream, callback) {
         var hash = crypto.createHash('sha1');
         hash.setEncoding('hex');
+
         stream.pipe(hash, {end: false});
         stream.on('end', function() {
             hash.end();
             var checkSum = hash.read();
-            
+
             callback(null, checkSum);
         });
         stream.on('error', function(error) {
             callback('error streaming file ' + error);
         });
     }
-    
+
     return {
         uploadFile: function(fileDir, fileName, objectId, mimeType, callback) {
             var filepath = fileDir + '/' + fileName;
 
-            fs.readFile(filepath, function(err, data){
-                
-                if(err){
+            fs.readFile(filepath, function(err, data) {
+
+                if (err) {
                     grunt.log.error('unable to read file', filepath);
                     // ignore this error and continue wiht next file
                     callback();
-                    return;                    
+                    return;
                 }
-                
+
                 var overwriteFlag = true;
                 cmisSession.setContentStream(objectId, data, overwriteFlag, mimeType).ok(function() {
                     grunt.log.ok("uploaded", filepath);
@@ -114,43 +114,67 @@ module.exports = function(cmisSession, options) {
                 });
             });
         },
-
         downloadFile: function(fileDir, fileName, objectId, mimeType, callback) {
             var filePath = fileDir + '/' + fileName;
 
             grunt.file.mkdir(fileDir);
-            compare(filePath, objectId, function(err, isSame){
-                if(err || isSame){
+
+
+
+
+            getRemoteData(objectId, function(err, response) {
+                if (err) {
                     callback(err);
                     return;
                 }
-                
-                getRemoteData(objectId, function(err, response) {
-                    if(err){
-                        callback(err);
-                        return;
-                    }
-                    if (response.statusCode !== 200) {
-                        grunt.log.error('Download failed', response.statusCode, filePath)
-                        callback();
-                    } else {
-                        response.pipe(fs.createWriteStream(filePath));
-                        response.on('end', function() {
-                            
+                if (response.statusCode !== 200) {
+                    grunt.log.error('Download failed', response.statusCode, filePath)
+                    callback();
+                } else {
+                    
+                    var bufferWriter =  new BufferWriter();
+                    //response.pipe(process.stdout);
+                    response.pipe(bufferWriter);
+                    
+                    // this will callback when response stream is exhausted
+                    compare(response, filePath, function(err, isSame) {
+
+                        if (err || isSame) {
+                            callback(err);
+                            return;
+                        }
+                        
+                        // if not the same - write buffer to a file 
+                        var writer = fs.createWriteStream(filePath);
+                        writer.write(bufferWriter.buffer, function(){
                             grunt.log.ok('downloaded', filePath);
                             callback(null);
                         });
-                        response.on('error', function() {
-                            callback('error streaming file ' + filePath);
-                        });
+                        
+                        writer.on('error', function(error){
+                            callback('error writing file ' + filePath + ' ' + error);
+                        })
+                    });
 
-                    }
-                });
+
+                }
             });
 
         }
     };
 };
+
+util.inherits(BufferWriter, Writable);
+
+function BufferWriter(){
+    Writable.call(this, {});
+    this.buffer = new Buffer(0);
+}
+
+BufferWriter.prototype._write = function(chunk, encoding, callback){
+    this.buffer = Buffer.concat([this.buffer, chunk]);
+    callback();
+}
 
 
 
